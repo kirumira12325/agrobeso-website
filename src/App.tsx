cecfc766a09d31614d928f56174991161128f980
kirumira12325 <kirumira12325@gmail.com>
2026-05-02 20:21:57 +0100

    Implement routing with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage';
+import Admin from '@/pages/Admin';
 
-const App = () => <HomePage />;
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
