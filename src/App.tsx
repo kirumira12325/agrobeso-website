7f349ef48997255830fb2f32be5f23ec8c6732ab
kirumira12325 <kirumira12325@gmail.com>
2026-05-27 12:43:57 +0100

    feat: add /reserve route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage';
 import Admin from '@/pages/Admin';
+import ReservePage from '@/pages/ReservePage';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/reserve" element={<ReservePage />} />
     </Routes>
   </BrowserRouter>
 );
